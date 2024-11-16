0aeb9a3bcdb0f2687a8e33393ce9d2de3b965471
Shashi Prasad <112333067+b2Shashi-mc@users.noreply.github.com>
2024-11-16 11:48:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
-        // Handle selection of Data View and display relevant code
-        function onDataViewSelect(dataView) {
-            document.getElementById('selectedDataView').innerText = dataView;
-            let code = "";
+// Handle selection of Data View and display relevant code
+function onDataViewSelect(dataView) {
+    document.getElementById('selectedDataView').innerText = dataView;
+    let code = "";
 
-            try {
-                if (dataView === "Sent") {
-                    code = generateSentCode();
-                } else if (dataView === "Open") {
-                    code = generateOpenCode();
-                } else if (dataView === "Bounce") {
-                    code = generateBounceCode();
-                } else if (dataView === "Click") {
-                    code = generateClickCode();
-                } else if (dataView === "Unsubscribe") {
-                    code = generateUnsubscribeCode();
-                }  else if (dataView === "Complaint") {
-                    code = generateComplaintCode();
-                } else if (dataView === "Subscribers") {
-                    code = generateSubscribersCode();
-                } else if (dataView === "ListSubscribers") {
-                    code = generateListSubscribersCode();
-                }else {
-                    throw new Error("Invalid Data View selected");
-                }
-
-                document.getElementById('code-editor').innerText = code;
-                document.getElementById('code-editor').textContent = decodeHTML(code);
-                document.querySelector('.code-editor-container').style.display = 'block';
-                Prism.highlightAll(); // Syntax highlighting
-
-            } catch (ex) {
-                showError(`Error loading code: ${ex.message}`);
-            }
+    try {
+        if (dataView === "Sent") {
+            code = generateSentCode();
+        } else if (dataView === "Open") {
+            code = generateOpenCode();
+        } else if (dataView === "Bounce") {
+            code = generateBounceCode();
+        } else if (dataView === "Click") {
+            code = generateClickCode();
+        } else if (dataView === "Unsubscribe") {
+            code = generateUnsubscribeCode();
+        } else if (dataView === "Complaint") {
+            code = generateComplaintCode();
+        } else if (dataView === "Subscribers") {
+            code = generateSubscribersCode();
+        } else if (dataView === "ListSubscribers") {
+            code = generateListSubscribersCode();
+        } else if (dataView === "BusinessUnitUnsubscribes") {
+            code = generateBusinessUnitUnsubscribesCode();
+        } else {
+            throw new Error("Invalid Data View selected");
         }
 
-        function generateSentCode() {
-            return `
+        document.getElementById('code-editor').innerText = code;
+        document.getElementById('code-editor').textContent = decodeHTML(code);
+        document.querySelector('.code-editor-container').style.display = 'block';
+        Prism.highlightAll(); // Syntax highlighting
+
+    } catch (ex) {
+        showError(`Error loading code: ${ex.message}`);
+    }
+}
+
+function generateSentCode() {
+    return `
 < script runat="server" >
     // Load necessary libraries
     Platform.Load("core", "1");
@@ -112,10 +114,84 @@
         return Stringify(result);
     }
 </ script >`;
+}
+function generateBusinessUnitUnsubscribesCode() {
+    return `
+<script runat="server">
+    // Load necessary libraries
+    Platform.Load("core", "1");
+
+    try {
+        // Define Data Extension and folder names
+        var dataExtensionName = "Dataview_BusinessUnitUnsubscribes"; // Name of the Data View
+        var folderName = "Data View"; // Folder where the Data Extension will be created
+
+        // Create Data Extension and retrieve result
+        var result = createDataExtension(dataExtensionName, folderName);
+        // Write result to console
+        Write(result);
+    } catch (ex) {
+        // Catch and log any errors that occur
+        Write(Stringify(ex));
+    }
+
+    // Function to retrieve the ID of a folder by name
+    function RetrieveFolderID(folderName) {
+        var folderID = null;
+        // Define filter to retrieve folder by name
+        var filter = {
+            Property: "Name",
+            SimpleOperator: "equals",
+            Value: folderName
+        };
+        // Retrieve folder based on filter
+        var folders = Folder.Retrieve(filter);
+        // If folder is found, set folderID
+        if (folders && folders.length > 0) {
+            folderID = folders[0].ID;
+        }
+        return folderID;
+    }
+
+    // Function to create a Data Extension
+    function createDataExtension(dataExtensionName, folderName) {
+        // Initialize WSProxy API
+        var api = new Script.Util.WSProxy();
+
+        // Set the client ID for API request
+        api.setClientId({ "ID": Platform.Function.AuthenticatedMemberID() });
+
+        // Retrieve folder ID using provided folderName
+        var folderID = RetrieveFolderID(folderName);
+        // If folder not found, throw error
+        if (!folderID) {
+            throw new Error("Folder not found: " + folderName);
         }
 
-        function generateOpenCode() {
-            return `
+        // Define Data Extension configuration
+        var config = {
+            "CustomerKey": dataExtensionName,
+            "Name": dataExtensionName,
+            "CategoryID": folderID, // Assign folderID to Data Extension
+            "Fields": [
+                // Define fields for BusinessUnitUnsubscribes Data Extension
+                { "CustomerKey": "BusinessUnitID", "Name": "BusinessUnitID", "FieldType": "Number", "IsPrimaryKey": false, "IsRequired": false },
+                { "CustomerKey": "SubscriberID", "Name": "SubscriberID", "FieldType": "Number", "IsPrimaryKey": false, "IsRequired": false },
+                { "CustomerKey": "SubscriberKey", "Name": "SubscriberKey", "FieldType": "Text", "MaxLength": 254, "IsPrimaryKey": false, "IsRequired": false },
+                { "CustomerKey": "UnsubDateUTC", "Name": "UnsubDateUTC", "FieldType": "Date", "IsPrimaryKey": false, "IsRequired": false },
+                { "CustomerKey": "UnsubReason", "Name": "UnsubReason", "FieldType": "Text", "MaxLength": 100, "IsPrimaryKey": false, "IsRequired": false }
+            ]
+        };
+
+        // Create the Data Extension using WSProxy API and return the result
+        var result = api.createItem("DataExtension", config);
+        return Stringify(result);
+    }
+</script>
+`;
+}
+function generateOpenCode() {
+    return `
 <script runat="server">
     // Load necessary libraries
     Platform.Load("core", "1");
@@ -195,11 +271,11 @@
     }
 </script>
 `;
-        }
+}
 
 
-        function generateBounceCode() {
-            return `
+function generateBounceCode() {
+    return `
 <script runat="server">
     // Load necessary libraries
     Platform.Load("core", "1");
@@ -289,10 +365,10 @@
     }
 </script>
 `;
-        }
+}
 
-        function generateClickCode() {
-            return `
+function generateClickCode() {
+    return `
 <script runat="server">
     // Load necessary libraries
     Platform.Load("core", "1");
@@ -375,10 +451,10 @@
     }
 </script>
  `;
-        }
+}
 
-        function generateComplaintCode() {
-            return `
+function generateComplaintCode() {
+    return `
 <script runat="server">
     // Load necessary libraries
     Platform.Load("core", "1");
@@ -456,10 +532,10 @@
     }
 </script>
  `;
-        }
+}
 
-        function generateUnsubscribeCode() {
-            return `
+function generateUnsubscribeCode() {
+    return `
 <script runat="server">
     // Load necessary libraries
     Platform.Load("core", "1");
@@ -536,10 +612,10 @@
         return Stringify(result);
     }
 </script>`;
-        }
+}
 
-        function generateSubscribersCode() {
-            return `
+function generateSubscribersCode() {
+    return `
 <script runat="server">
     // Load necessary libraries
     Platform.Load("core", "1");
@@ -617,10 +693,10 @@
         return Stringify(result);
     }
 </script>`;
-        }
+}
 
-        function generateListSubscribersCode() {
-            return `
+function generateListSubscribersCode() {
+    return `
 <script runat="server">
     // Load necessary libraries
     Platform.Load("core", "1");
@@ -699,69 +775,69 @@
         return Stringify(result);
     }
 </script>`;
-        }
+}
 
-        // Decodes HTML entities
-        function decodeHTML(html) {
-            const txt = document.createElement("textarea");
-            txt.innerHTML = html;
-            return txt.value;
-        }
+// Decodes HTML entities
+function decodeHTML(html) {
+    const txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
 
-        // Show error message to the user
-        function showError(message) {
-            const errorDiv = document.createElement("div");
-            errorDiv.className = "error-toast";
-            errorDiv.innerText = message;
-            document.body.appendChild(errorDiv);
-            setTimeout(() => {
-                errorDiv.remove();
-            }, 5000);
-        }
+// Show error message to the user
+function showError(message) {
+    const errorDiv = document.createElement("div");
+    errorDiv.className = "error-toast";
+    errorDiv.innerText = message;
+    document.body.appendChild(errorDiv);
+    setTimeout(() => {
+        errorDiv.remove();
+    }, 5000);
+}
 
-        // Show success message (for copying code)
-        function showSuccess(message) {
-            const successDiv = document.createElement("div");
-            successDiv.className = "success-toast";
-            successDiv.innerText = message;
-            document.body.appendChild(successDiv);
-            setTimeout(() => {
-                successDiv.remove();
-            }, 5000);
-        }
+// Show success message (for copying code)
+function showSuccess(message) {
+    const successDiv = document.createElement("div");
+    successDiv.className = "success-toast";
+    successDiv.innerText = message;
+    document.body.appendChild(successDiv);
+    setTimeout(() => {
+        successDiv.remove();
+    }, 5000);
+}
 
-        // Copy code to clipboard
-        function copyCode() {
-            const code = document.getElementById('code-editor').innerText;
-            navigator.clipboard.writeText(code).then(() => {
-                showSuccess("Code copied to clipboard!");
-            }).catch(() => {
-                showError("Failed to copy code.");
-            });
-        }
+// Copy code to clipboard
+function copyCode() {
+    const code = document.getElementById('code-editor').innerText;
+    navigator.clipboard.writeText(code).then(() => {
+        showSuccess("Code copied to clipboard!");
+    }).catch(() => {
+        showError("Failed to copy code.");
+    });
+}
 
-        // Event Listener for DOM Content
-        document.addEventListener('DOMContentLoaded', () => {
-            // Get the first radio button in the .data-view-list
-            const firstRadioButton = document.querySelector('.data-view-list input[type="radio"]');
-        
-            if (firstRadioButton) {
-                firstRadioButton.checked = true; // Set the first radio button as checked by default
-            }
-        
-            // Optional: If you want to trigger any event (e.g., change) after selecting the first item
-            if (firstRadioButton) {
-                const changeEvent = new Event('change');
-                firstRadioButton.dispatchEvent(changeEvent);
-            }
-        
-            // You can add more JavaScript logic here for other functionalities if needed
-        });
-document.addEventListener('contextmenu', function(event) {
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the first radio button in the .data-view-list
+    const firstRadioButton = document.querySelector('.data-view-list input[type="radio"]');
+
+    if (firstRadioButton) {
+        firstRadioButton.checked = true; // Set the first radio button as checked by default
+    }
+
+    // Optional: If you want to trigger any event (e.g., change) after selecting the first item
+    if (firstRadioButton) {
+        const changeEvent = new Event('change');
+        firstRadioButton.dispatchEvent(changeEvent);
+    }
+
+    // You can add more JavaScript logic here for other functionalities if needed
+});
+
+document.addEventListener('contextmenu', function (event) {
     event.preventDefault();
 }, false);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     if (event.ctrlKey && (event.key === 'u' || event.key === 'U')) {
         event.preventDefault();
     }
